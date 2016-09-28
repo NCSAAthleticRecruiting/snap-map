8cf677ee5d356f6b2b2e52fdebd0464dbe245860
Jake Nations <jnations@8thlight.com>
2016-09-28 11:22:58 -0500

    Create css and components folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './map';
-import './index.css';
+import Map from './components/map';
+import '../public/css/index.css';
 
 ReactDOM.render(
   <Map />,
