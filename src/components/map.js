908cb6382c1e01b545d71f9b50448eec1e3b7d3c
Joshua Lockhart <jlockhart@ncsasports.org>
2016-09-28 13:15:30 -0500

    added some classes necessary for map markers

--- src/components/map.js
+++ src/components/map.js
@@ -2,9 +2,12 @@ import React, {PropTypes, Component} from 'react';
 
 import GoogleMap from 'google-map-react';
 
+var chicago = {lat: 41.881832, lng: -87.623177}
+var philadelphia = {lat: 39.9526, lng: -75.1652}
+
 export default class Map extends Component {
   static defaultProps = {
-    center: {lat: 41.881832, lng: -87.623177},
+    center: chicago,
     zoom: 9,
   };
 
@@ -17,7 +20,9 @@ export default class Map extends Component {
        <GoogleMap
         bootstrapURLKeys={{key: 'AIzaSyDp6OGS8beEflySUYkEFLQ1iT2_QwaVl8I'}}
         defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}>
+        defaultZoom={this.props.zoom}
+        onChange={this.props.onChange}
+        onClick={this.props.onClick}>
       </GoogleMap>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from './components/map';
+import Marker, {K_SCALE_NORMAL} from './components/marker';
 import '../public/css/index.css';
 
+function _onClick(obj){ console.log(obj.x, obj.y, obj.lat, obj.lng, obj.event);}
+
 ReactDOM.render(
-  <Map />,
+  <Map onClick={_onClick} />,
   document.getElementById('root')
 );
